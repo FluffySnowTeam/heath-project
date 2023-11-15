8c206e913fd23de7b67f015ed0429951501127d6
죠 <stop.ho@khu.ac.kr>
2023-11-15 11:07:05 +0900

    design: 디테일페이지 퍼블리싱 완료

---
+++ heath/src/app/detailpage/_mocks/detail.mocks.ts
@@ -0,0 +1,30 @@
+import profileImage from '/public/images/testProfileImage.png'
+export const replyMockData = [
+  {
+    id: 1,
+    content: '너무 이쁘네요 어디인가요?',
+    created: new Date(),
+    user: {
+      id: '이영민',
+      profileImage: profileImage,
+    },
+  },
+  {
+    id: 2,
+    content: '저도 산책 가고싶네용',
+    created: new Date(),
+    user: {
+      id: '이영민',
+      profileImage: profileImage,
+    },
+  },
+  {
+    id: 3,
+    content: '굿굿국',
+    created: new Date(),
+    user: {
+      id: '이영민',
+      profileImage: profileImage,
+    },
+  },
+]
--- heath/src/app/mypage/page.tsx
+++ heath/src/app/mypage/page.tsx
@@ -1,8 +1,6 @@
 'use client'
-import Image from 'next/image'
 import * as S from './MyPage.styles'
 import { Goals, InfoModal, GoalModal } from './_components'
-import { useState } from 'react'
 
 const MyPage = () => {
   const UserInfo = (
